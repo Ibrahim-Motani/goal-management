a40fdc9ea7ff3d0223532ec4b395d08a4f64c283
Ibrahim-Motani <ibhrahim.motani220@gmail.com>
2022-04-19 13:15:19 +0530

    Initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Type check TypeScript files
+  "**/*.(ts|tsx)": () => "npx tsc --noEmit",
+
+  // Lint and format TypeScript and JS files
+  "**/*.(ts|tsx|js)": (filenames) => [
+    `npx eslint --fix ${filenames.join(" ")}`,
+    `npx prettier --write ${filenames.join(" ")}`
+  ],
+
+  // Format MarkDown and JSON
+  "**/*.(md|json)": (filenames) => `npx prettier --write ${filenames.join(" ")}`
+};
